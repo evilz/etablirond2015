939f66cc780fa05a374a5c355c0d0847328de91a
Juliette Tisseyre <juliette.tisseyre@gmail.com>
2015-03-12 20:16:43 +0100

    computation of capacity done

---
+++ capacity.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import file_handler as fh
+
+def getGlobalCapacity(servs, group):
+    capacity = 0
+    for serv in servs:
+         if serv['group'] == group:
+             capacity += serv['capacity']
+    return capacity
+
+def getMinCapacityByGroup(datacenter, servs, group):
+    globalCapacity = getGlobalCapacity(servs, group)
+    minCapacity = globalCapacity
+
+    for row in range(len(datacenter)):
+        currCapacity = globalCapacity
+        for serv in servs:
+            if serv['group'] == group and serv['pos'][0] == row :
+                currCapacity -= serv['capacity']
+        minCapacity = min(minCapacity, currCapacity)
+
+    return minCapacity
+
+def getMinCapacity(datacenter, servs, nbGroups):
+    minCapacity = 0
+
+    for group in range(nbGroups):
+        currCapacity = getMinCapacityByGroup(datacenter, servs, group)
+        minCapacity = min(minCapacity, currCapacity)
+
+    return minCapacity
+
+if __name__ == "__main__":
+  (datacenter, servs, nbGroups) = fh.readfile("./input.txt")
+  print getMinCapacity(datacenter, servs, nbGroups)
