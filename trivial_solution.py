5ec72325e70f2878788a5f5f00e9f8822fe85f15
Rémi Labeyrie <remi.labeyrie@scmfrance.fr>
2015-03-12 20:18:43 +0100

    it works

--- trivial_solution.py
+++ trivial_solution.py
@@ -11,7 +11,10 @@ for (index, server) in enumerate(servers):
             if space == Enum.EMPTY:
                 match = True
                 for j in range(server['size']):
-                    if row[y + j] != Enum.EMPTY:
+                    if y + j < len(row):
+                        if row[y + j] != Enum.EMPTY:
+                            match = False
+                    else:
                         match = False
                 if match:
                     location = (x, y)
@@ -19,6 +22,8 @@ for (index, server) in enumerate(servers):
         if match:
             break
     if match:
+        server['used'] = True
+        server['pos'] = location
         for i in range(server['size']):
             datacenter[location[0]][location[1] + i] = index
 
@@ -28,10 +33,10 @@ sortedServers = sorted(servers, key=lambda x: x['capacity'])
 
 groups = list(0 for i in range(nbgroups))
 
-for (index, server) in sortedServers:
-    if sortedServers['used']:
+for (index, server) in enumerate(sortedServers):
+    if server['used']:
         groupIndex = groups.index(min(groups))
         groups[groupIndex] += server['capacity']
-        servers[server['index']]['group'] = groupIndex
+        servers[server['id']]['group'] = groupIndex
 
 writesoluce("output.txt", servers)
