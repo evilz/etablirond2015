6b871652f5544518f8c92d0c7ba0a5daadfc3b2b
lamiomni <lamiomni@users.noreply.github.com>
2015-03-13 14:50:50 +0100

    Latest version

--- DatacenterOptimizer/Visu/Form1.Designer.cs
+++ DatacenterOptimizer/Visu/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Visu
+namespace Visu
 {
     partial class Form1
     {
@@ -10,7 +10,7 @@
         /// <summary>
         /// Nettoyage des ressources utilisées.
         /// </summary>
-        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -34,18 +34,20 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1001, 164);
+            this.pictureBox1.Size = new System.Drawing.Size(1006, 160);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1025, 190);
+            this.ClientSize = new System.Drawing.Size(1025, 181);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Datacenter Visualizer";
             this.Load += new System.EventHandler(this.Form1_Load);
