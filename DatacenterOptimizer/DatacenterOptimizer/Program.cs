db5669e0cb20d6a376e57d9750cbb4e9f9c1d071
Tony Nguyen <ngn.tony@gmail.com>
2015-03-13 01:45:56 +0100

    Vizualizer

--- DatacenterOptimizer/DatacenterOptimizer/Program.cs
+++ DatacenterOptimizer/DatacenterOptimizer/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace DatacenterOptimizer
 {
-    class Pool
+    public class Pool
     {
+        public static Pool EmptyPool = new Pool(-1);
         public int Number;
         public int Capacity;
 
@@ -17,7 +19,7 @@ namespace DatacenterOptimizer
         }
     }
 
-    class Datacenter
+    public class Datacenter
     {
         public static KeyValuePair<int, int> EmptyChunk = new KeyValuePair<int, int>(-1, -1);
         public readonly Server[] Cells;
@@ -35,7 +37,9 @@ namespace DatacenterOptimizer
 
         public void SetUnavailable(int index)
         {
-            Cells[index] = Server.Unavailable;
+            var s = new Server(1, -1, -1) {Pool = Pool.EmptyPool, Position = index};
+            Cells[index] = s;
+
         }
 
         public void ComputeChunks()
@@ -100,9 +104,9 @@ namespace DatacenterOptimizer
         }
     }
 
-    class Server
+    public class Server
     {
-        public static Server Unavailable = new Server(-1, -1, -1);
+        //public static Server Unavailable = new Server(1, -1, -1) { Pool = Pool.EmptyPool };
 
         public int Number;
         public int Capacity;
@@ -120,45 +124,143 @@ namespace DatacenterOptimizer
             Ratio = capacity/(double)size;
         }
 
+        public static Brush GetColor(Server s)
+        {
+            if (s == null || s.Pool == null)
+            {
+                return Brushes.Black;
+            }
+
+            switch (s.Pool.Number)
+            {
+                case -1:
+                    return Brushes.Red;
+                case 0:
+                    return Brushes.Yellow;
+                case 1:
+                    return Brushes.Aqua;
+                case 2:
+                    return Brushes.Aquamarine;
+                case 3:
+                    return Brushes.YellowGreen;
+                case 4:
+                    return Brushes.Violet;
+                case 5:
+                    return Brushes.Tomato;
+                case 6:
+                    return Brushes.Lime;
+                case 7:
+                    return Brushes.SlateBlue;
+                case 8:
+                    return Brushes.Blue;
+                case 9:
+                    return Brushes.BlueViolet;
+                case 10:
+                    return Brushes.Brown;
+                case 11:
+                    return Brushes.SkyBlue;
+                case 12:
+                    return Brushes.CadetBlue;
+                case 13:
+                    return Brushes.Chartreuse;
+                case 14:
+                    return Brushes.Chocolate;
+                case 15:
+                    return Brushes.Coral;
+                case 16:
+                    return Brushes.CornflowerBlue;
+                case 17:
+                    return Brushes.Plum;
+                case 18:
+                    return Brushes.Crimson;
+                case 19:
+                    return Brushes.Cyan;
+                case 20:
+                    return Brushes.DarkBlue;
+                case 21:
+                    return Brushes.DarkCyan;
+                case 22:
+                    return Brushes.DarkGoldenrod;
+                case 23:
+                    return Brushes.DarkGray;
+                case 24:
+                    return Brushes.DarkGreen;
+                case 25:
+                    return Brushes.DarkKhaki;
+                case 26:
+                    return Brushes.DarkMagenta;
+                case 27:
+                    return Brushes.DarkOliveGreen;
+                case 28:
+                    return Brushes.DarkOrange;
+                case 29:
+                    return Brushes.DarkOrchid;
+                case 30:
+                    return Brushes.DarkRed;
+                case 31:
+                    return Brushes.DarkSalmon;
+                case 32:
+                    return Brushes.DarkSeaGreen;
+                case 33:
+                    return Brushes.DarkSlateBlue;
+                case 34:
+                    return Brushes.DarkSlateGray;
+                case 35:
+                    return Brushes.DarkTurquoise;
+                case 36:
+                    return Brushes.DarkViolet;
+                case 37:
+                    return Brushes.DeepPink;
+                case 38:
+                    return Brushes.DeepSkyBlue;
+                case 39:
+                    return Brushes.DimGray;
+                case 40:
+                    return Brushes.DodgerBlue;
+                case 41:
+                    return Brushes.Firebrick;
+                case 42:
+                    return Brushes.Orange;
+                case 43:
+                    return Brushes.ForestGreen;
+                default: // 44
+                    return Brushes.Fuchsia;
+            }
+        }
+
         public new string ToString()
         {
             return Datacenter == null ? "x" : string.Format("{0} {1} {2}", Datacenter.Number, Position, Pool.Number);
         }
     }
 
-    class Program
+    public class Program
     {
         private static void Main(string[] args)
         {
-            Tuple<Datacenter[], Server[], Pool[]> parsed = Parse();
             var sb2 = new StringBuilder();
+            Tuple<Datacenter[], Server[], Pool[]> parsed = Parse();
 
-            // Place servers
+            PlaceServers(parsed);
+            PlacePools(parsed, sb2);
+            GetMinCap(parsed.Item1, parsed.Item3);
+
             /*
-            while (true)
-            {
-                var maxAvailableChunk = parsed.Item1.Select(d => d.ChunkMax.Value).Max();
-                var datacenterWithMaxChunk = parsed.Item1.First(d => d.ChunkMax.Value == maxAvailableChunk);
-                var availableServers = parsed.Item2.Where(s => s.Datacenter == null && s.Size <= maxAvailableChunk);
+            var sw = new StreamWriter("res.out");
+            var sw2 = new StreamWriter("res.debug");
+            sw.Write(sb.ToString());
+            sw2.Write(sb2.ToString());
+            sw.Close();
+            sw2.Close();*/
 
-                if (!availableServers.Any())
-                {
-                    break;
-                }
+            Console.ReadLine();
+        }
 
-                var maxAvailableCapacity = availableServers.Max(s => s.Ratio);
-                Server server = availableServers.FirstOrDefault(s => s.Ratio == maxAvailableCapacity);
 
-                if (server == null)
-                {
-                    break;
-                }
-
-                datacenterWithMaxChunk.SetServer(server);
-                sb2.AppendFormat("Server {0} placed\r\n", server.Number);
-            }*/
-
+        public static void PlacePools(Tuple<Datacenter[], Server[], Pool[]> parsed, StringBuilder sb2)
+        {
             // Place pools
+            /*
             var servers = parsed.Item2.Where(s => s.Datacenter != null).OrderBy(s => s.Capacity).Reverse();
             
             foreach (var server in servers)
@@ -172,6 +274,30 @@ namespace DatacenterOptimizer
                 server.Pool = selectedPool;
                 selectedPool.Capacity += server.Capacity;
                 sb2.AppendFormat("Server {0} assigned to pool {1}\r\n", server.Number, selectedPool.Number);
+            }*/
+
+            /*
+            int dataIndex = 0;
+            foreach (var pool in parsed.Item3)
+            {
+                Server t = parsed.Item1[dataIndex++ % parsed.Item1.Length].Cells.Where(s => s != null && s != Server.Unavailable && s.Pool == null).OrderBy(s => s.Capacity).First();
+
+                t.Pool = pool;
+                pool.Capacity += t.Capacity;
+                sb2.AppendFormat("Server {0} assigned to pool {1}\r\n", t.Number, pool.Number);
+            }*/
+
+            foreach (var datacenter in parsed.Item1)
+            {
+                foreach (
+                    var server in datacenter.Cells.Where(s => s != null && s.Pool == null).Distinct()
+                    )
+                {
+                    Pool selectedPool = GetMinCap(parsed.Item1, parsed.Item3);
+                    server.Pool = selectedPool;
+                    selectedPool.Capacity += server.Capacity;
+                    sb2.AppendFormat("Server {0} assigned to pool {1}\r\n", server.Number, selectedPool.Number);
+                }
             }
 
             var sb = new StringBuilder();
@@ -179,19 +305,9 @@ namespace DatacenterOptimizer
             {
                 sb.AppendLine(server.ToString());
             }
-
-            var sw = new StreamWriter("res.out");
-            var sw2 = new StreamWriter("res.debug");
-            sw.Write(sb.ToString());
-            sw2.Write(sb2.ToString());
-            sw.Close();
-            sw2.Close();
-
-            GetMinCap(parsed.Item1, parsed.Item3);
-            Console.ReadLine();
         }
 
-        private static Pool GetMinCap(Datacenter[] dcs, Pool[] ps)
+        public static Pool GetMinCap(Datacenter[] dcs, Pool[] ps)
         {
             int globalminCap = int.MaxValue;
             Pool minPool = null;
@@ -202,7 +318,7 @@ namespace DatacenterOptimizer
                 {
                     var cap = dcs.Where(dc => dc != datacenter).Sum(
                         dc =>
-                            dc.Cells.Where(s => s != null && s != Server.Unavailable && s.Pool == pool)
+                            dc.Cells.Where(s => s != null && s.Pool == pool)
                                 .Distinct()
                                 .Sum(s => s.Capacity));
                     if (minCap > cap)
@@ -248,7 +364,7 @@ namespace DatacenterOptimizer
             return minPool;
         }
 
-        private static Tuple<Datacenter[], Server[], Pool[]> Parse()
+        public static Tuple<Datacenter[], Server[], Pool[]> Parse()
         {
             var sr = new StreamReader("dc.in");
             string[] inputs = sr.ReadToEnd().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
@@ -295,35 +411,65 @@ namespace DatacenterOptimizer
                 int y = int.Parse(data[1]);
                 servers[i] = new Server(x, y, i);
             }
+            sr.Close();
+            return Tuple.Create(datacenters, servers, pools);
+        }
+
+        public static void PlaceServers(Tuple<Datacenter[], Server[], Pool[]> tuple)
+        {
+            // Place servers
+            /*
+            while (true)
+            {
+                var maxAvailableChunk = parsed.Item1.Select(d => d.ChunkMax.Value).Max();
+                var datacenterWithMaxChunk = parsed.Item1.First(d => d.ChunkMax.Value == maxAvailableChunk);
+                var availableServers = parsed.Item2.Where(s => s.Datacenter == null && s.Size <= maxAvailableChunk);
+
+                if (!availableServers.Any())
+                {
+                    break;
+                }
+
+                var maxAvailableCapacity = availableServers.Max(s => s.Ratio);
+                Server server = availableServers.FirstOrDefault(s => s.Ratio == maxAvailableCapacity);
+
+                if (server == null)
+                {
+                    break;
+                }
+
+                datacenterWithMaxChunk.SetServer(server);
+                sb2.AppendFormat("Server {0} placed\r\n", server.Number);
+            }*/
 
             // Preparse
             var sr2 = new StreamReader("output.txt");
-            inputs = sr2.ReadToEnd().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            index = 0;
+            string[] inputs = sr2.ReadToEnd().Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            int index = 0;
 
-            foreach (var server in servers)
+            foreach (var server in tuple.Item2)
             {
-                input = inputs[index++];
-                data = input.Split();
+                string input = inputs[index++];
+                string[] data = input.Split();
 
                 if (data.Length == 3)
                 {
                     // "x" : string.Format("{0} {1} {2}", Datacenter.Number, Position, Pool.Number);
-                    datacenters[int.Parse(data[0])].SetServer(server, int.Parse(data[1]));
+                    tuple.Item1[int.Parse(data[0])].SetServer(server, int.Parse(data[1]));
                 }
             }
 
-            for (int i = 0; i < numberOfDatacenter; i++)
+            for (int i = 0; i < tuple.Item1.Length; i++)
             {
-                datacenters[i].ComputeChunks();
+                tuple.Item1[i].ComputeChunks();
             }
 
-            for (int i = 0; i < poolSize; i++)
+            for (int i = 0; i < tuple.Item3.Length; i++)
             {
-                pools[i] = new Pool(i);
+                tuple.Item3[i] = new Pool(i);
             }
 
-            return Tuple.Create(datacenters, servers, pools);
+            sr2.Close();
         }
     }
 }
---
+++ DatacenterOptimizer/Visu/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using DatacenterOptimizer;
+
+namespace Visu
+{
+    public partial class Form1 : Form
+    {
+        private Tuple<Datacenter[], Server[], Pool[]> _data;
+        private Graphics _flagGraphics;
+        private Bitmap _flag;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var sb2 = new StringBuilder();
+
+            _data = DatacenterOptimizer.Program.Parse();
+            DatacenterOptimizer.Program.PlaceServers(_data);
+            DatacenterOptimizer.Program.PlacePools(_data, sb2);
+            DatacenterOptimizer.Program.GetMinCap(_data.Item1, _data.Item3);
+            int width = _data.Item1[0].Cells.Length * 10, height = _data.Item1.Length * 10;
+            pictureBox1.Size = new Size(width, height);
+            Controls.Add(pictureBox1);
+            _flag = new Bitmap(width, height);
+            _flagGraphics = Graphics.FromImage(_flag);
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < _data.Item1.Length; i++)
+                {
+                    var datacenter = _data.Item1[i];
+                    var servers = datacenter.Cells.Distinct().ToList();
+                    for (int j = 0; j < servers.Count; j++)
+                    {
+                        var server = servers[j];
+                        _flagGraphics.FillRectangle(Server.GetColor(server), server.Position * 10, i * 10, 10 * server.Size, 10);
+                        try
+                        {
+                            pictureBox1.Image = _flag;
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                        Thread.Sleep(1);
+                    }
+                }
+            }).Start();
+        }
+    }
+}
