cd68b93da3ea80485d576f03f5981cf0387af30d
lamiomni <lamiomni@users.noreply.github.com>
2015-03-13 15:20:51 +0100

    Update Form1.cs

--- DatacenterOptimizer/Visu/Form1.cs
+++ DatacenterOptimizer/Visu/Form1.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using DatacenterOptimizer;
@@ -11,8 +9,6 @@ namespace Visu
     public partial class Form1 : Form
     {
         private Tuple<Datacenter[], Server[], Pool[]> _data;
-        private Graphics _flagGraphics;
-        private Bitmap _flag;
 
         public Form1()
         {
@@ -21,49 +17,14 @@ namespace Visu
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            _data = DatacenterOptimizer.Program.Parse();
-            int width = _data.Item1[0].Cells.Length * 10, height = _data.Item1.Length * 10;
-            pictureBox1.Size = new Size(width, height);
-            Controls.Add(pictureBox1);
-            _flag = new Bitmap(width, height);
-            _flagGraphics = Graphics.FromImage(_flag);
-            _flagGraphics.FillRectangle(Brushes.Black, 0, 0, width, height);
-            pictureBox1.Image = _flag;
-
             var sb2 = new StringBuilder();
 
+            _data = DatacenterOptimizer.Program.Parse();
             DatacenterOptimizer.Program.PlaceServers(_data, sb2);
             DatacenterOptimizer.Program.PlacePools(_data, sb2);
-            int counter = DatacenterOptimizer.Program.GetMinCap(_data, true).Item3;
-
-            for (int i = 0; i < _data.Item1.Length; i++)
-            {
-                var datacenter = _data.Item1[i];
-                var servers = datacenter.Cells.Distinct().ToList();
-
-                for (int j = 0; j < servers.Count; j++)
-                {
-                    var server = servers[j];
-
-                    try
-                    {
-                        _flagGraphics.FillRectangle(Server.GetColor(server), server.Position * 10, i * 10, 10 * server.Size, 10);
-                    }
-                    catch (Exception)
-                    {
-                    }
-                }
-            }
-
-            try
-            {
-                pictureBox1.Image = _flag;
-                _flag.Save("Visu_" + counter + ".png", ImageFormat.Png);
-            }
-            catch (Exception)
-            {
-
-            }
+            Bitmap image = DatacenterOptimizer.Program.GetMinCap(_data, true).Item3;
+            pictureBox1.Size = new Size(image.Width, image.Height);
+            pictureBox1.Image = image;
         }
     }
 }
